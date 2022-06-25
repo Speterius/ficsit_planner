71bea8c0468de20b0210425292f304a8cc81890f
Peter <peet.seres@gmail.com>
2022-06-25 17:27:08 +0200

    Added an example react flow component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
 import Header from "./components/Header";
+import BasicFlow from "./components/ExampleFlow";
+import ReactFlowProvider from 'react-flow-renderer';
+
+// function App() {
+//   return (
+//     <div className='container'>
+//       <Header/>
+//       <ReactFlowProvider>
+//         <Flow/>
+//       </ReactFlowProvider>
+//     </div>
+//   )
+// }
 
 function App() {
   return (
     <div className='container'>
       <Header/>
+      <div style={{ height: 800 }}>
+        <ReactFlowProvider>
+          <BasicFlow/>
+        </ReactFlowProvider>
+      </div>
     </div>
   )
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -2,6 +2,7 @@ const Header = ({title}) => {
   return (
     <header>
         <h1> {title} </h1>
+        <p1> Welcome to Ficsit Planner (wip). </p1>
     </header>
   )
 }
